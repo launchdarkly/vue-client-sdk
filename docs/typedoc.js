5b10fed68b3afd39a0f89e3e0ab8b500cde93af9
Christie Williams <xie.williams@gmail.com>
2022-06-30 17:05:52 -0400

    Releaser updates

--- docs/typedoc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  out: '/tmp/project-releaser/project/docs/build/html',
-  exclude: ['**/node_modules/**'],
-  name: 'LaunchDarkly Vue SDK (1.0.0)',
-  readme: 'none', // don't add a home page with a copy of README.md
-  entryPoints: '/tmp/project-releaser/project/src/index.ts',
-  entryPointStrategy: 'expand',
-}
