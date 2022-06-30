8b3a4cd195e01ff350793e640c9a3269c6ed484e
Christie Williams <xie.williams@gmail.com>
2022-06-30 15:57:05 -0400

    Update sample app

--- example/App.vue
+++ example/App.vue
@@ -1,12 +1,17 @@
-<script setup>
-  import Feature from './Feature.vue'
-  import { ref } from 'vue'
-  const show = ref(true)
+<script setup lang="ts">
+import { useLdReady, useLdFlag } from '../src'
+
+// Set featureFlagKey to the feature flag key you want to evaluate
+const featureFlagKey = 'my-boolean-flag'
+
+const ldReady = useLdReady()
+const flagValue = useLdFlag(featureFlagKey, false /* default flag value */)
 </script>
 
 <template>
-  <button @click="show = !show">Show/Hide</button>
-  <div v-if="show">
-    <Feature />
+  <div v-if="ldReady">
+    SDK successfully initialized! Feature flag '{{ featureFlagKey }}' is {{ flagValue }}
+    for this user.
   </div>
+  <div v-else>LaunchDarkly client initializing...</div>
 </template>
--- example/main.js
+++ example/main.js
@@ -5,10 +5,15 @@ import { LDPlugin } from '../src'
 // Set clientSideID to your LaunchDarkly client-side ID
 const clientSideID = ''
 
-// Set up the user properties. This user should appear on your LaunchDarkly
-// users dashboard soon after you run the demo.
-const user = { key: 'example-user-key', name: 'Sandy' }
+if (!clientSideID) {
+  // eslint-disable-next-line no-undef
+  console.warn('Please edit main.js to set clientSideID to your LaunchDarkly client-side ID')
+} else {
+  // Set up the user properties. This user should appear on your LaunchDarkly
+  // users dashboard soon after you run the demo.
+  const user = { key: 'example-user-key', name: 'Sandy' }
 
-const app = createApp(App)
-app.use(LDPlugin, { clientSideID, user })
-app.mount('#app')
+  const app = createApp(App)
+  app.use(LDPlugin, { clientSideID, user })
+  app.mount('#app')
+}
