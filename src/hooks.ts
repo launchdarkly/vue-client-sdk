c2339641715250f8d98cef319fd38129cc176bef
Christie Williams <xie.williams@gmail.com>
2022-06-30 12:06:16 -0400

    Typedoc annotations

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,5 +1,5 @@
 import { inject, type InjectionKey } from 'vue'
-import { LD_CLIENT, LD_FLAG, LD_INIT, LD_READY } from '.'
+import { LD_CLIENT, LD_FLAG, LD_INIT, LD_READY, type LDPluginOptions } from '.'
 
 const safeInject = <T>(key: InjectionKey<T>, label: string): T => {
   const target = inject(key)
@@ -9,10 +9,44 @@ const safeInject = <T>(key: InjectionKey<T>, label: string): T => {
   return target
 }
 
-export const useLdReady = () => safeInject(LD_READY, 'LD_READY')
+/**
+ * Indicates if the LaunchDarkly client has finished initializing. Uses Vue's inject API, and will only
+ * work if run inside a Vue setup hook or `<script setup>`.
+ *
+ * @return Readonly boolean reference indicating if the LaunchDarkly client has finished initializing.
+ */
+export function useLdReady() {
+  return safeInject(LD_READY, 'LD_READY')
+}
 
-export const useLdClient = () => safeInject(LD_CLIENT, 'LD_CLIENT')
+/**
+ * Provides the LaunchDarkly javascript client,
+ * {@link https://launchdarkly.github.io/js-client-sdk/interfaces/_launchdarkly_js_client_sdk_.ldclient.html}.
+ * Uses Vue's inject API, and will only work if run inside a Vue setup hook or `<script setup>`.
+ */
+export function useLdClient() {
+  return safeInject(LD_CLIENT, 'LD_CLIENT')
+}
 
-export const ldInit = () => safeInject(LD_INIT, 'LD_INIT')
+/**
+ * Initializes the LaunchDarkly client.
+ * Uses Vue's inject API, and will only work if run inside a Vue setup hook or `<script setup>`.
+ */
+export function ldInit(initOptions: LDPluginOptions) {
+  return safeInject(LD_INIT, 'LD_INIT')(initOptions)
+}
 
-export const useLdFlag = <T>(flagKey: string, defaultValue?: T) => safeInject(LD_FLAG, 'LD_FLAG')(flagKey, defaultValue)
+/**
+ * Use to reference a single feature flag. Will automatically subsciribe to streamed updates
+ * unless the `streaming` option was set to false.
+ * Uses Vue's inject API, so will only work if run inside a Vue setup hook or `<script setup>`.
+ *
+ * @typeParam T - Type of the flag's value. Can be inferred if a default value is provided.
+ * @param flagKey Key of the feature flag to be referenced.
+ * @param defaultValue Default value to be used while flag value loads, or if flag cannot be found.
+ *
+ * @return Readonly ref to the flag's value.
+ */
+export function useLdFlag<T>(flagKey: string, defaultValue?: T) {
+  return safeInject(LD_FLAG, 'LD_FLAG')(flagKey, defaultValue)
+}
--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,69 @@ import { readonly, ref, type InjectionKey, type Ref, type App } from 'vue'
 import { getLdFlag, type FlagRef } from './getLdFlag'
 export { useLdReady, useLdFlag, ldInit, useLdClient } from './hooks'
 
-type LDInitOptions = {
+export type LDPluginOptions = {
+  /**
+   * Indicates which LaunchDarkly project to use. Must be provided here or in a call to {@link ldInit} for the SDK to work.
+   */
   clientSideID?: string | undefined
+  /**
+   * User definition.
+   *
+   * @defaultValue Defaults to anonymous user, `{ anonymous: 'true' }`
+   */
   user?: LDUser | undefined
+  /**
+   * Use to disable automatically subscribing to live updates to flags referenced using {@link useLdFlag}.
+   *
+   * @defaultValue `true`
+   */
   streaming?: boolean
+  /**
+   * Use to defer initialization of the LaunchDarkly client until {@link ldInit} is called explicitly.
+   *
+   * @defaultValue `false`
+   */
   deferInitialization?: boolean
+  /**
+   * Options to pass to the underlying javascript SDK.
+   */
   options?: LDOptions | undefined
 }
 
-type LDPluginOptions = LDInitOptions // can be extended with vue plugin specific options
-
-export const LD_INIT = Symbol() as InjectionKey<(o?: LDInitOptions) => [LDClient, Readonly<Ref<boolean>>]>
+/**
+ * Injection key used to retreive LaunchDarkly client initialization function. Usage: `const ldInit = inject(LD_INIT)`.
+ * Alternatively use {@link ldInit}.
+ */
+export const LD_INIT = Symbol() as InjectionKey<(o?: LDPluginOptions) => [LDClient, Readonly<Ref<boolean>>]>
+/**
+ * Injection key used to retreive a boolean ref indicating if the LaunchDarkly client has finirshed initializing.
+ * Usage: `const ldReady = inject(LD_READY)`.
+ * Alternatively use {@link useLdReady}.
+ */
 export const LD_READY = Symbol() as InjectionKey<Readonly<Ref<boolean>>>
+/**
+ * Injection key used to retreive LaunchDarkly client. Usage: `const ldclient = inject(LD_CLIENT)`.
+ * Alternatively use {@link useLdClient}.
+ */
 export const LD_CLIENT = Symbol() as InjectionKey<LDClient>
+/**
+ * Injection key used to retreive `ldFlag` function. Usage: `const ldFlag = inject(LD_FLAG)`.
+ * Alternatively use {@link useLdFlag}.
+ */
 export const LD_FLAG = Symbol() as InjectionKey<<T>(flagKey: string, defaultFlagValue?: T | undefined) => FlagRef<T>>
 
+/**
+ * Vue plugin wrapper for the LaunchDarkly javascript SDK.
+ *
+ * If provided with a clientSideID will initialize the LaunchDarkly client automatically (unless `deferInitialization` is true).
+ *
+ * @see LDPluginOptions for configuration options
+ */
 export const LDPlugin = {
   install(app: App, pluginOptions: LDPluginOptions = {}) {
     const ldReady = ref(false)
     const $ldReady = readonly(ldReady)
-    const $ldInit = (initOptions: LDInitOptions = {}): [LDClient, Readonly<Ref<boolean>>] => {
+    const $ldInit = (initOptions: LDPluginOptions = {}): [LDClient, Readonly<Ref<boolean>>] => {
       const clientSideID = initOptions.clientSideID ?? pluginOptions.clientSideID
       if (clientSideID === undefined) {
         throw new Error(`Cannot initialize LaunchDarkly without a clientSideID`)
