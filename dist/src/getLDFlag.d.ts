d7da2f968e4201ad632579952651bc633573310c
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2022-07-13 19:44:36 +0000

    Updating documentation to version 1.0.0

--- dist/src/getLDFlag.d.ts
+++
@@ -1,4 +0,0 @@
-import { type Ref } from 'vue';
-import type { LDClient } from 'launchdarkly-js-client-sdk';
-export declare type FlagRef<T> = Readonly<Ref<T>>;
-export declare const getLDFlag: (isLdReady: boolean, $ldClient: LDClient, enableStreaming?: boolean) => <T>(flagKey: string, defaultFlagValue?: T | undefined) => Readonly<Ref<T>>;
