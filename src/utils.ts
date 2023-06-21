929239d781c2eea9057fd42e5deb34531f7c11a2
Yusinto Ngadiman <yus@launchdarkly.com>
2023-06-21 15:09:14 -0700

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -4,5 +4,8 @@ import type { LDPluginOptions } from './index'
 /**
  * Helper function to get the context or fallback to classic user.
  * Safe to remove when the user property is deprecated.
+ *
+ * Ripped from react sdk:
+ * https://github.com/launchdarkly/react-client-sdk-private/blob/a66fcdfd626c367e446c7c6f65cb9360f8dfad36/src/utils.ts#L9
  */
 export const getContextOrUser = (config: LDPluginOptions): LDContext | undefined => config.context ?? config.user
