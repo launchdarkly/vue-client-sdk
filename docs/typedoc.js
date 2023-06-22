ae4bd8b5fa4bc6d69d8f5422968f1284ce8ef3bb
Yusinto Ngadiman <yusinto@gmail.com>
2023-06-22 10:07:24 -0700

    Update typedoc.js

--- docs/typedoc.js
+++ docs/typedoc.js
@@ -4,7 +4,7 @@ module.exports = {
     '**/node_modules/**',
     'test-types.ts'
   ],
-  name: "LaunchDarkly Vue SDK (1.0.0)",
+  name: "LaunchDarkly Vue SDK (2.0.0)",
   readme: 'none',                // don't add a home page with a copy of README.md
   entryPoints: "/tmp/project-releaser/project/src/index.ts",
   entryPointStrategy: "expand"
