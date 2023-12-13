adcb3a39e60f8604c0a9708550e8688796e76ab7
Yusinto Ngadiman <yusinto@gmail.com>
2023-12-13 11:13:34 -0800

    fix: add type module to package.json. improve example app.

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,15 +1,15 @@
-<script setup lang="ts">
-import { ldInit, useLDFlag } from '../src'
+<script setup>
+import { ldInit, useLDFlag } from 'launchdarkly-vue-client-sdk'
 
 // Set clientSideID to your LaunchDarkly client-side ID
-const clientSideID = 'CLIENT-SIDE-ID'
+const clientSideID = 'client-side-id'
 
 // Set up the context properties. This context should appear on your LaunchDarkly
 // contexts dashboard soon after you run the demo.
 const context = { kind:'user', key: 'example-context-key', name: 'Sandy' }
 
 // Set featureFlagKey to the feature flag key you want to evaluate
-const featureFlagKey = 'my-boolean-flag'
+const featureFlagKey = 'dev-test-flag'
 
 if (!clientSideID) {
   console.warn('Please edit App.vue to set clientSideID to your LaunchDarkly client-side ID')
@@ -20,8 +20,20 @@ const flagValue = useLDFlag(featureFlagKey, false)
 
 <template>
   <div v-if="ldReady">
-    SDK successfully initialized! Feature flag '{{ featureFlagKey }}' is {{ flagValue }}
-    for this context.
+    <p>Welcome to LaunchDarkly. The Vue client SDK has been initialized.</p>
+    <p>Feature flag '{{ featureFlagKey }}' is {{ flagValue }}
+    for this context.</p>
   </div>
   <div v-else>LaunchDarkly client initializing...</div>
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
